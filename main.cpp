a133c7399dcb2c03b89a4bd68b222029d1aa01f5
Ábrahám Zsolt <abraham.zsolt@hallgato.sze.hu>
2026-03-01 11:32:38 +0100

    Bugok javítása

--- main.cpp
+++ main.cpp
@@ -4,24 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // BUG: NELEMENTS helyett N_ELEMENTS kell (hiányzik az aláhúzás)
-    std::cout << '1-100 ertekek duplazasa' // BUG: single quote helyett idézőjel kell, hiányzik a << std::endl;
-    for (int i = 0;) // BUG: hiányzik a feltétel (i < N_ELEMENTS) és az inkrementálás (i++)
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) // BUG: feltétel hibás, i helyett i < N_ELEMENTS kell
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" // BUG: hiányzik << b[i] << std::endl; és a pontosvessző
+        std::cout << "Ertek: " << b[i] << std::endl;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; // BUG: nincs inicializálva, int atlag = 0; kellene
-    for (int i = 0; i < N_ELEMENTS, i++) // BUG: vessző helyett pontosvessző kell
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i] // BUG: hiányzik a pontosvessző
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    // BUG: hiányzik a memória felszabadítás: delete[] b;
+    delete[] b;
     return 0;
 }
